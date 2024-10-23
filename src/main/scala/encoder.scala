9fb0b884c3c48332d005db8b834402deb095aa43
Pawel Stawicki <pawelstawicki@gmail.com>
2024-10-23 23:25:47 +0200

    Encoder for nested case class

--- src/main/scala/encoder.scala
+++ src/main/scala/encoder.scala
@@ -1,36 +1,22 @@
 import scala.compiletime.{constValue, erasedValue, summonInline}
 import scala.deriving.Mirror
 
-case class User(name: String, age: Int)
+case class User(name: String, age: Int, address: Address)
 
 case class Address(country: String, city: String, street: String, number: Int)
-
-trait Show[A] {
-  val typeName: String
-  def show(a: A): String
-}
-
-object Show {
-  given Show[String] with
-    val typeName = "String"
-    def show(a: String): String = a
-
-
-  given Show[Int] with
-    val typeName = "Int"
-    def show(a: Int): String = a.toString
-}
 
 trait Encoder[A] {
   def encode(a: A): String
 }
 
-@main
-def runEncoder(): Unit = {
-  val v = Address("Polska", "Szczecin", "Kwiatowa", 10)
-  val enc: Encoder[Address] = summon[Encoder[Address]]
-  println(enc.encode(v))
+object Encoder {
+    given Encoder[String] with
+        def encode(a: String): String = a
+
+    given Encoder[Int] with
+        def encode(a: Int): String = a.toString
 }
+
 
 inline implicit def makeEncoder[A](using m: Mirror.Of[A]): Encoder[A] = (a: A) => {
   val labels = deconstructLabels[m.MirroredElemLabels]
@@ -38,7 +24,7 @@ inline implicit def makeEncoder[A](using m: Mirror.Of[A]): Encoder[A] = (a: A) =
   val memberTypes = deconstructMemberTypes[m.MirroredElemTypes]
 
   val lines = a.asInstanceOf[Product].productIterator.zip(labels).zip(memberTypes).map {
-    case ((value, label), show) => s"$label: ${show.show(value)}"
+    case ((value, label), show) => s"$label: ${show.encode(value)}"
   }
 
   constValue[m.MirroredLabel] + "\n" + lines.mkString("\n")
@@ -48,8 +34,15 @@ inline def deconstructLabels[T <: Tuple]: List[String] = inline erasedValue[T] m
   case _: (head *: tail) => constValue[head].asInstanceOf[String] :: deconstructLabels[tail]
   case _: EmptyTuple => List.empty
 
-inline def deconstructMemberTypes[T <: Tuple]: List[Show[Any]] = inline erasedValue[T] match
+inline def deconstructMemberTypes[T <: Tuple]: List[Encoder[Any]] = inline erasedValue[T] match
   case _: (head *: tail) =>
-    val show = summonInline[Show[head]]
-    show.asInstanceOf[Show[Any]] :: deconstructMemberTypes[tail]
+    val show = summonInline[Encoder[head]]
+    show.asInstanceOf[Encoder[Any]] :: deconstructMemberTypes[tail]
   case _: EmptyTuple => List.empty
+
+@main
+def runEncoder(): Unit = {
+  val v = User("Pawel", 45, Address("Polska", "Szczecin", "Kwiatowa", 10))
+  val enc = summon[Encoder[User]]
+  println(enc.encode(v))
+}
